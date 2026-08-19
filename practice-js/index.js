cdfbf6d2648c6bf7df6612c147c4bec7a661315a
Raj Kumar Das <devsionraj@hotmail.com>
2026-08-19 09:20:58 +0530

    type conversion and type coercion practice

---
+++ practice-js/index.js
@@ -0,0 +1,23 @@
+/*const age = 19;
+const isOldEnough = age >= 18;
+
+if (isOldEnough) {
+    console.log(`sarah can start driving`);
+} else {
+    console.log(`Sarah needs to wait for ${18 - age} yrs before applying for a DL`)
+}*/
+
+/*
+// Type Conversion
+const inputYear = '1991';
+console.log(inputYear + 1);
+console.log(Number(inputYear) + 1);
+
+console.log(Number('ansd'));
+console.log(String(24), 24);
+
+// Type Coercion
+console.log('23' - '10');
+console.log('23' - 10);
+console.log('23' - '10' - 3);
+*/
