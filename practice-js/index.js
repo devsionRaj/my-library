b09dfacf1308215e486e2f23b39de3fcd5c8cd67
Raj Kumar Das <devsionraj@hotmail.com>
2026-08-19 20:52:43 +0530

    truthy and falsy

--- practice-js/index.js
+++ practice-js/index.js
@@ -21,3 +21,25 @@ console.log('23' - '10');
 console.log('23' - 10);
 console.log('23' - '10' - 3);
 */
+
+/*
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean(''));
+console.log(Boolean('Raj'));
+console.log(Boolean({}));
+
+const money = 100;
+if (money) {
+    console.log(`Don't spend it all!`);
+} else {
+    console.log(`You should get a job!`);
+}
+
+let height;
+if (height) {
+    console.log(`YAY! Height is defined!`);
+} else {
+    console.log(`Height is UNDEFINED!`);
+}
+*/
